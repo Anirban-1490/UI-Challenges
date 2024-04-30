47a72d67c7e1f43515027e15cc2f597c3644cd5f
Anirban Pratihar <anirbanpratihar45@gmail.com>
2024-04-30 16:56:59 +0530

    pathc: route to rating challange, Ui style refactored

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { Rating } from "@/Components/Rating-system";
 import { SortableList } from "@/Components/Sortable-list";
 import { useState } from "react";
 
-const components = [{ id: 1, component: SortableList }];
+const components = [
+  { id: 1, component: SortableList, label: "Sortable List" },
+  { id: 2, component: Rating, label: "Rating" },
+];
 
 export default function Home() {
   const [selectedComponent, setComponent] = useState<number | null>(null);
@@ -14,21 +18,32 @@ export default function Home() {
           UI Challenges
         </h1>
       )}
-      <section className="mx-auto  w-[85%] ">
-        {!selectedComponent && (
-          <div className="mt-10">
-            <button
-              onClick={() => setComponent(1)}
-              className="px-2 py-8 w-[16rem] border-2 border-white/15 rounded-md transition-all hover:shadow-md hover:shadow-white/50"
-            >
-              <h3 className="  text-3xl font-sans">Sortable List</h3>
-            </button>
-          </div>
-        )}
+      <section
+        className={`mx-auto  w-[85%] ${
+          !selectedComponent ? "flex gap-10" : ""
+        }`}
+      >
+        {!selectedComponent &&
+          components.map((comp) => {
+            return (
+              <div key={comp.id} className="mt-10">
+                <button
+                  onClick={() => setComponent(comp.id)}
+                  className="px-2 py-8 w-[16rem] border-2 border-white/15 rounded-md transition-all hover:shadow-md hover:shadow-white/50"
+                >
+                  <h3 className="  text-3xl font-sans">{comp.label}</h3>
+                </button>
+              </div>
+            );
+          })}
         {selectedComponent &&
           components.map((component) => {
             if (component.id == selectedComponent) {
-              return <component.component key={component.id} />;
+              return (
+                <div className="mt-10" key={component.id}>
+                  <component.component />
+                </div>
+              );
             }
           })}
       </section>
