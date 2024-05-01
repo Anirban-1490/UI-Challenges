73e8329ad2ab29295d970d06e88f3ecc18d89a50
Anirban Pratihar <anirbanpratihar45@gmail.com>
2024-05-01 12:37:34 +0530

    feat: added rating descriptions

--- Components/Rating-system/index.tsx
+++ Components/Rating-system/index.tsx
@@ -1,11 +1,11 @@
 import { StarFilled } from "@ant-design/icons";
 import { useState } from "react";
 import { v4 as uuidV4 } from "uuid";
+import { ratingDescription } from "./rating-text";
 
 export const Rating = () => {
   const [hoveredElement, setHoveredElement] = useState<number | null>(null);
   const [rating, setRating] = useState<number | null>(null);
-  console.log(rating);
 
   return (
     <>
@@ -15,11 +15,14 @@ export const Rating = () => {
       <div
         className=" flex gap-6 justify-center w-fit mx-auto"
         onMouseLeave={() => {
+          //* when mouse exist from parent container , rmeove the hovered element counter
           setHoveredElement(null);
         }}
       >
         {new Array(5).fill("").map((id, index) => {
+          //* check when hovering over a star, if this hovered star and previous stars can be filled
           const isFilledOver = hoveredElement && hoveredElement >= index + 1;
+          //* check when clicking on a star, if this star where the click happens and previous stars can be filled
           const isFilledOnrating = rating && rating >= index + 1;
 
           const isFilled = isFilledOver || isFilledOnrating;
@@ -42,6 +45,15 @@ export const Rating = () => {
           );
         })}
       </div>
+      {rating && (
+        <div className="mt-10 mx-auto ">
+          <p className=" text-center px-[20rem]">
+            {ratingDescription.find(
+              (ratingDesc) => ratingDesc.rating === rating
+            )?.text || ""}
+          </p>
+        </div>
+      )}
     </>
   );
 };
---
+++ Components/Rating-system/rating-text.ts
@@ -0,0 +1,22 @@
+export const ratingDescription = [
+  {
+    rating: 1,
+    text: "How many stars would you give to our Online Code Editor? We're sorry to hear that you had a bad experience. We would like to learn more about what happened and how we can make things right.",
+  },
+  {
+    rating: 2,
+    text: "We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.",
+  },
+  {
+    rating: 3,
+    text: "Thank you for your feedback. We're sorry to hear that your experience wasn't perfect. We would love to hear more about your concerns to see how we can improve.",
+  },
+  {
+    rating: 4,
+    text: "Thank you for your positive feedback! We're glad to know that you had a great experience and we appreciate your support.",
+  },
+  {
+    rating: 5,
+    text: "Excellent! We're thrilled to hear you had such a positive experience. Thank you for choosing our platform",
+  },
+];
